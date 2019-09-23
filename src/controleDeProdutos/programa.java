c1294f987424faf5c6af54f6c2ea2c40f366d903
jr17061991leo <jr17061991leo@gmail.com>
2019-09-23 19:06:24 -0300

    criacao de gets e sets

--- src/controleDeProdutos/programa.java
+++ src/controleDeProdutos/programa.java
@@ -11,18 +11,20 @@ public class programa {
 
 		Locale.setDefault(Locale.US);
 		Scanner in = new Scanner(System.in);
-		int n=0;
-		Produto[] prod = new Produto[n];
-		
+
 		System.out.println("digite a quantidades de produtos para leitura:");
-		n = in.nextInt();
-		
+		int n = in.nextInt();
+		Produto[] prod = new Produto[n];
+
 		for (int i = 0; i < prod.length; i++) {
+			in.nextLine();
 			System.out.println("digite o nome e preço do produto: ");
-			
-			
+			String nome = in.nextLine();
+			float preço = in.nextFloat();
+			prod[i] = new Produto(nome, preço);
+
 		}
-		
+
 		in.close();
 	}
 
--- src/entidades/Produto.java
+++ src/entidades/Produto.java
@@ -1,12 +1,29 @@
 package entidades;
 
 public class Produto {
-	String nome;
-	float preço;
+	private String nome;
+	private float preço;
 
 	public Produto(String nome, float preço) {
 		this.nome = nome;
 		this.preço = preço;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getPreço() {
+		return preço;
+	}
+
+	public void setPreço(float preço) {
+		this.preço = preço;
+	}
+	
 
 }
